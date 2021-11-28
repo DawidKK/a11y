64bc1212266931546c8ac5a9e0e3d4e527d0893e
Dawid Kawiak <d.kawiak@selleo.com>
2021-11-28 17:34:42 +0100

    Add axe core

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (process.env.NODE_ENV !== 'production') {
+  import('react-axe').then(axe => {
+    axe.default(React, ReactDOM, 1000);
+    ReactDOM.render(<App />, document.getElementById('root'));
+  });
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
